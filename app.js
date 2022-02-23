b3e7c68119ed94d02fe2fac35f0e4f14691fb8b0
JimmiJan <96634490+JimmiJan@users.noreply.github.com>
2022-02-23 11:37:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,105 @@
+// alert("jamshed Shahid")
+
+
+// LotMoreToPracticeForJavascript.pdf
+
+
+
+// reverce is a usning for revrece value and text
+
+
+// function reverce(){
+//   let rev = prompt("Enter Your Number")
+//   let d= rev.split('').reverse().join("")
+//   document.write(d)
+// }
+
+
+// function ort(str){
+//     // 
+//  return str.split('').sort().join('');
+ 
+
+// }
+// document.write(ort("zimbabe" + " " ))
+
+
+// function alphabet_order(tr)
+//   {
+// return tr.split('').sort().join('');
+//   }
+// document.write(alphabet_order(prompt("webmaster") + " " ));
+
+
+
+
+
+// function vowel(str1)
+// {
+//     let vowell = "aeiouAEIOU"
+//     let vCount = 0
+//  for (let i = 0; i < str1.length; i++)
+//   {
+//     if(vowell.indexOf(str1[i]) !== -1){
+//         vCount += 1
+//   }
+     
+//  }
+//  return vCount
+// }
+// document.write(vowel("Thanks for signing up for CodePen!"))
+
+// function vowel_count(str1)
+// {
+//   var vowel_list = 'aeiouAEIOU';
+//   var vcount = 0;
+  
+//   for(var x = 0; x < str1.length ; x++)
+//   {
+//     if (vowel_list.indexOf(str1[x]) !== -1)
+//     {
+//       vcount += 1;
+//     }
+  
+//   }
+//   return vcount;
+// }
+// console.log(vowel_count(prompt("VALUE ADD")));
+
+
+// for (let index = 1; index < 10; index++) {
+//     document.write(index);
+    
+// }
+
+// for (let index = 1; index < 100; index = index+2) {
+//     document.write(index);
+    
+// }
+
+// for (let i= 1; i<=10; i++){
+//     var row = "7 * " + i +"=" + 7 *i + " <br />"
+//     document.write(row);
+// }
+
+// for (let i = 1 ; i<=10 ; i++)
+// {
+//     table(i )
+//     // document.write("")
+// }
+// function table(n)
+// {
+//     for (let i= 1; i<=10; i++){
+//         var row = n + "*" + i + "=" + n *i + " <br />"
+//         document.write(row);
+//     }
+// }
+
+
+// var sum = 0
+// for (let i = 1; i <=10 ; i++)
+// {
+//     sum += i
+    
+// }
+// document.write(sum)
